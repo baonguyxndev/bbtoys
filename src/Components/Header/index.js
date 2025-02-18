771f97ea22a06a00abec4d7c179f7258a88e3cde
yonB280405 <giabao2005bbb@gmail.com>
2025-02-18 15:05:17 +0700

    first commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="top-strip">
+          <div className="container">
+            <p className="mb-0 mt-0 text-center">test test test</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
